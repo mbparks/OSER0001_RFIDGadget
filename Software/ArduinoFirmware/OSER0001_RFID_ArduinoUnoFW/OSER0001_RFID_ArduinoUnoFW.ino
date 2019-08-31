09a62854f385c968ffe196bb2fee26e1f0d27528
mike parks <mbparks@gmail.com>
2019-08-30 22:27:41 -0400

    Switched to an array of pointers to Adafruit_PN532 objects

--- Software/ArduinoFirmware/OSER0001_RFID_ArduinoUnoFW/OSER0001_RFID_ArduinoUnoFW.ino
+++ Software/ArduinoFirmware/OSER0001_RFID_ArduinoUnoFW/OSER0001_RFID_ArduinoUnoFW.ino
@@ -1,36 +1,29 @@
-/**************************************************************************/
-/*! 
-    @file     readMifareClassic.pde
-    @author   Adafruit Industries
-  @license  BSD (see license.txt)
+/**************************************************************************
+  File:    OSER0001_RFID_ArduinoUnoFW
+  Author:  mbparks
+  Date:    30AUG2019
+  Ver.:    0.0.1
+  License: BSD
 
-    This example will wait for any ISO14443A card or tag, and
-    depending on the size of the UID will attempt to read from it.
-   
-    If the card has a 4-byte UID it is probably a Mifare
-    Classic card, and the following steps are taken:
-   
-    Reads the 4 byte (32 bit) ID of a MiFare Classic card.
-    Since the classic cards have only 32 bit identifiers you can stick
-  them in a single variable and use that to compare card ID's as a
-  number. This doesn't work for ultralight cards that have longer 7
-  byte IDs!
-   
-    Note that you need the baud rate to be 115200 because we need to
-  print out the data and read from the card at the same time!
+  This code is four gadget that can be used in an escape room .
+  It will activate a relay once all RFID tagged objects are placed
+  over the correct PN532 RFID readers.
+  Supports 1 through 4 RFID readers.
 
-This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
-This library works with the Adafruit NFC breakout 
+  Based on example code for PN532 by Adafruit Industries
+  
+  This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
+  This library works with the Adafruit NFC breakout
   ----> https://www.adafruit.com/products/364
- 
-Check out the links above for our tutorials and wiring diagrams 
-These chips use SPI to communicate, 4 required to interface
 
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-*/
-/**************************************************************************/
+  Check out the links above for our tutorials and wiring diagrams
+  These chips use SPI to communicate, 4 required to interface
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+**************************************************************************/
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
@@ -45,138 +38,134 @@ products from Adafruit!
 //#define PN532_SS3  6
 //#define PN532_SS4  5
 
+#define NUMREADERS 2
 
 #define DEBUG
 
 #ifdef DEBUG
-#define DEBUG_PRINT(x)  Serial.println (x)
+#define DEBUG_PRINT(x)  Serial.print (x)
 #else
 #define DEBUG_PRINT(x)
 #endif
 
+#ifdef DEBUG
+#define DEBUG_PRINTLN(x)  Serial.println (x)
+#else
+#define DEBUG_PRINT(x)
+#endif
 
-Adafruit_PN532 nfc1(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS1);
-Adafruit_PN532 nfc2(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS2);
-//Adafruit_PN532 nfc3(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS3);
-//Adafruit_PN532 nfc4(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS4);
+Adafruit_PN532 *nfcReaders[NUMREADERS];
+
+const uint32_t ssPins[NUMREADERS] = {PN532_SS1, PN532_SS2};  //PN532_SS3, PN532_SS4};
+const uint32_t nfcAcceptedTags[NUMREADERS] = {3872679160, 3584511166};   //0, 0};
+
+bool isLocked = true;
 
 
-const uint32_t nfcAcceptedTagId1 = 3872679160;
-const uint32_t nfcAcceptedTagId2 = 3584511166;
-//const uint32_t nfcAcceptedTagId3 = 0;
-//const uint32_t nfcAcceptedTagId4 = 0;
-
+/**************************************************************************
+ * Function: setup
+ * Inputs:   None
+ * Outputs:  None
+ **************************************************************************/
 
 void setup(void) {
   Serial.begin(115200);
-  Serial.println("Welcome to the Escape Room RFID Gadget!");
+  Serial.println(F("Welcome to the Escape Room RFID Gadget!"));
 
-  nfc1.begin();
-  nfc2.begin();
-  //nfc3.begin();
-  //nfc4.begin();
-
-  getNfcVersion(&nfc1);
-  getNfcVersion(&nfc2);
-  //getNfcVersion(&nfc3);
-  //getNfcVersion(&nfc4);
-
-  nfc1.SAMConfig();
-  nfc2.SAMConfig();
-  //nfc3.SAMConfig();
-  //nfc4.SAMConfig();
-
-  nfc1.setPassiveActivationRetries(1);
-  nfc2.setPassiveActivationRetries(1);
-  //nfc3.setPassiveActivationRetries(1);
-  //nfc4.setPassiveActivationRetries(1);
-  
-  Serial.println("System initialized ...");
-}
-
-
-void loop(void) {
-
-  uint32_t nfcTag1 = 0;
-  uint32_t nfcTag2 = 0;
-  //uint32_t nfcTag3 = 0;
-  //uint32_t nfcTag4 = 0;
-  
-  DEBUG_PRINT("READER #1");
-  nfcTag1 = readNfc(&nfc1);
-  DEBUG_PRINT(nfcTag1);
-  delay(1000);
-  
-  DEBUG_PRINT("READER #2");
-  nfcTag2 = readNfc(&nfc2);
-  DEBUG_PRINT(nfcTag2);
-  delay(1000);
-
-  /************************
-  DEBUG_PRINT("READER #3");
-  nfcTag3 = readNfc(&nfc3);
-  DEBUG_PRINT(nfcTag3);
-  delay(1000);
-  
-  DEBUG_PRINT("READER #4");
-  nfcTag4 = readNfc(&nfc4);
-  DEBUG_PRINT(nfcTag4);
-  delay(1000);
-  ***********************/
-
-  if((nfcTag1 == nfcAcceptedTagId1) && (nfcTag2 == nfcAcceptedTagId2)) // && (nfcTag3 == nfcAcceptedTagId3) && (nfcTag4 == nfcAcceptedTagId4));
+  for (int x = 0; x < NUMREADERS; x++)
   {
-    DEBUG_PRINT("All Tags Correct. Activate Relay.");
+    nfcReaders[x] = new Adafruit_PN532(PN532_SCK, PN532_MISO, PN532_MOSI, ssPins[x]);
+    nfcReaders[x]->begin();
+    uint32_t versiondata = nfcReaders[x]->getFirmwareVersion();
+    if (! versiondata) {
+      Serial.print(F("Didn't find PN53x board"));
+      while (1); // halt
+    }
+    // Got ok data, print it out!
+    Serial.print(F("Found chip PN5"));
+    Serial.println((versiondata >> 24) & 0xFF, HEX);
+    Serial.print(F("Firmware ver. "));
+    Serial.print((versiondata >> 16) & 0xFF, DEC);
+    Serial.print('.');
+    Serial.println((versiondata >> 8) & 0xFF, DEC);
+
+    nfcReaders[x]->SAMConfig();
+    nfcReaders[x]->setPassiveActivationRetries(1);
   }
+
+  Serial.println(F("System initialized ..."));
 }
 
 
-void getNfcVersion(Adafruit_PN532 * nfc)
-{
-  uint32_t versiondata = nfc->getFirmwareVersion();
-  if (! versiondata) {
-    Serial.print("Didn't find PN53x board");
-    while (1); // halt
-  }
-  // Got ok data, print it out!
-  Serial.print("Found chip PN5"); 
-  Serial.println((versiondata>>24) & 0xFF, HEX); 
-  Serial.print("Firmware ver. "); 
-  Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); 
-  Serial.println((versiondata>>8) & 0xFF, DEC);
-}
 
+/**************************************************************************
+ * Function: loop
+ * Inputs:   None
+ * Outputs:  None
+ **************************************************************************/
+void loop(void) {
+  if (isLocked == true)
+  {
+    uint32_t readNfcTags[NUMREADERS] = {0, 0};
+    uint32_t countOfMatchingTags = 0;
 
+    for (int x = 0; x < NUMREADERS; x++) {
+      uint8_t success;
+      uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+      uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+      uint32_t cardid = 0;
 
-uint32_t readNfc(Adafruit_PN532 * nfc)
-{
-  uint8_t success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-  uint32_t cardid = 0;
-    
-  success = nfc->readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-  
-  if (success) {
-    // Display some basic information about the card
-    DEBUG_PRINT("Found an ISO14443A card");
-    DEBUG_PRINT("  UID Value: ");
-    nfc->PrintHex(uid, uidLength);
-    
-    if (uidLength == 4)
-    {
-      // We probably have a Mifare Classic card ... 
-      cardid = uid[0];
-      cardid <<= 8;
-      cardid |= uid[1];
-      cardid <<= 8;
-      cardid |= uid[2];  
-      cardid <<= 8;
-      cardid |= uid[3]; 
-      DEBUG_PRINT("Seems to be a Mifare Classic card.");
+      DEBUG_PRINT("READER #");
+      DEBUG_PRINTLN(x);
+
+      success = nfcReaders[x]->readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+      if (success) {
+        // Display some basic information about the card
+        DEBUG_PRINTLN(F("Found an ISO14443A card"));
+        DEBUG_PRINTLN(F("  UID Value: "));
+        nfcReaders[x]->PrintHex(uid, uidLength);
+
+        if (uidLength == 4)
+        {
+          // We probably have a Mifare Classic card ...
+          cardid = uid[0];
+          cardid <<= 8;
+          cardid |= uid[1];
+          cardid <<= 8;
+          cardid |= uid[2];
+          cardid <<= 8;
+          cardid |= uid[3];
+          DEBUG_PRINTLN(F("Seems to be a Mifare Classic card."));
+        }
+      }
+
+      DEBUG_PRINT(F("Tag ID: "));
+      DEBUG_PRINTLN(cardid);
+      readNfcTags[x] = cardid;
+      delay(1000);
+    }
+
+    for (int z = 0; z < NUMREADERS; z++) {
+      if (readNfcTags[z] == nfcAcceptedTags[z])
+      {
+        countOfMatchingTags++;
+      }
+    }
+
+    if (countOfMatchingTags == NUMREADERS) {
+      activateRelay();
     }
   }
-  
-  return cardid;
+}
+
+/**************************************************************************
+ * Function: activateRelay
+ * Inputs:   None
+ * Outputs:  None
+ **************************************************************************/
+void activateRelay()
+{
+  DEBUG_PRINTLN(F("ALERT!!!  ALERT!!! All Tags Correct. Activate Relay.  ALERT!!! ALERT !!!"));
+  isLocked = false;
 }
