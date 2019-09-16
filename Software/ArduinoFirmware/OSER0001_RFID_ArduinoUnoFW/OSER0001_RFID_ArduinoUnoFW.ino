8d83199ed0a1aa479b4a04456e7504465371cc0b
mike parks <mbparks@gmail.com>
2019-09-15 23:55:15 -0400

    document code

--- Software/ArduinoFirmware/OSER0001_RFID_ArduinoUnoFW/OSER0001_RFID_ArduinoUnoFW.ino
+++ Software/ArduinoFirmware/OSER0001_RFID_ArduinoUnoFW/OSER0001_RFID_ArduinoUnoFW.ino
@@ -28,7 +28,7 @@
 #include <SPI.h>
 #include <Adafruit_PN532.h>
 
-
+//DEBUG message
 #define DEBUG
 
 #ifdef DEBUG
@@ -40,11 +40,12 @@
 #ifdef DEBUG
 #define DEBUG_PRINTLN(x)  Serial.println (x)
 #else
-#define DEBUG_PRINT(x)
+#define DEBUG_PRINTLN(x)
 #endif
 
-
 const uint32_t NUM_READERS = 2;        //Adjust number as needed 1 to 4
+
+//Hardware GPIO pin assignments
 const uint32_t PN532_SCK_PIN =  13;
 const uint32_t PN532_MISO_PIN = 12;
 const uint32_t PN532_MOSI_PIN = 11;
@@ -52,24 +53,30 @@ const uint32_t PN532_SS1_PIN =  8;
 const uint32_t PN532_SS2_PIN =  7;
 const uint32_t PN532_SS3_PIN = 6;
 const uint32_t PN532_SS4_PIN = 5;
+const uint32_t CLOSED_LED_PIN = 4;
+const uint32_t PWM_OUT_PIN = 3;
+const uint32_t OPEN_LED_PIN = 2;
 
+//The tags that will allow device to open
 const uint32_t ACCEPTED_TAG_ID1 = 3872679160;
 const uint32_t ACCEPTED_TAG_ID2 = 3584511166;
 //const uint32_t ACCEPTED_TAG_ID3 = 0;
 //const uint32_t ACCEPTED_TAG_ID4 = 0;
 
-const uint32_t PWM_OUT_PIN = 3;
-const uint32_t OPEN_LED_PIN = 2;
-const uint32_t CLOSED_LED_PIN = 4;
+//PWM values for device locked (LOW) and device opened (HIGH)
 const uint32_t PWM_LOW_VAL = 0;
 const uint32_t PWM_HIGH_VAL = 255;
 
 const uint32_t DELAY_AMT = 1000;
-bool isLocked = true;
+bool isLocked = true; // Init system state as locked
 
+//An array of the chip select pins for each RFID reader
 const uint32_t SS_PINS[NUM_READERS] = {PN532_SS1_PIN, PN532_SS2_PIN};  //PN532_SS3_PIN, PN532_SS4_PIN};
+
+//An array of the accepted RFID tags for each RFID reader
 const uint32_t NFC_ACCEPTED_TAGS[NUM_READERS] = {ACCEPTED_TAG_ID1, ACCEPTED_TAG_ID2};   //ACCEPTED_TAG_ID3, ACCEPTED_TAG_ID4};
 
+//An array of RFID readers
 Adafruit_PN532 *nfcReaders[NUM_READERS];
 
 
@@ -80,7 +87,7 @@ Adafruit_PN532 *nfcReaders[NUM_READERS];
  **************************************************************************/
 void setup(void) {
   Serial.begin(115200);
-  Serial.println(F("Welcome to the Escape Room RFID Gadget v0.0.1!"));
+  DEBUG_PRINTLN(F("Welcome to the Escape Room RFID Gadget v0.0.1!"));
 
   for (int x = 0; x < NUM_READERS; x++)
   {
@@ -91,17 +98,17 @@ void setup(void) {
     uint32_t versiondata = 9999;
     while (versiondata == 9999) {
       versiondata = nfcReaders[x]->getFirmwareVersion();
-      Serial.print(F("Didn't find PN53x board #"));
-      Serial.println(x);
+      DEBUG_PRINT(F("Didn't find PN53x board #"));
+      DEBUG_PRINTLN(x);
     }
     
     // Got ok data, print it out!
-    Serial.print(F("Found chip PN5"));
-    Serial.println((versiondata >> 24) & 0xFF, HEX);
-    Serial.print(F("Firmware ver. "));
-    Serial.print((versiondata >> 16) & 0xFF, DEC);
-    Serial.print('.');
-    Serial.println((versiondata >> 8) & 0xFF, DEC);
+    DEBUG_PRINT(F("Found chip PN5"));
+    DEBUG_PRINTLN(String((versiondata >> 24) & 0xFF, HEX));
+    DEBUG_PRINT(F("Firmware ver. "));
+    DEBUG_PRINT(String((versiondata >> 16) & 0xFF, DEC));
+    DEBUG_PRINT('.');
+    DEBUG_PRINTLN(String((versiondata >> 8) & 0xFF, DEC));
 
     nfcReaders[x]->SAMConfig();
     nfcReaders[x]->setPassiveActivationRetries(1);
@@ -110,12 +117,11 @@ void setup(void) {
   pinMode(PWM_OUT_PIN, OUTPUT);
   pinMode(OPEN_LED_PIN, OUTPUT);
   pinMode(CLOSED_LED_PIN, OUTPUT);
-  
-  analogWrite(PWM_OUT_PIN, PWM_LOW_VAL);
-  digitalWrite(OPEN_LED_PIN, LOW);
-  digitalWrite(CLOSED_LED_PIN, HIGH);
 
-  Serial.println(F("System initialized ..."));
+  setDeviceOutputs();
+
+      //Device is locked and ready to go!
+      DEBUG_PRINTLN(F("SUCCESS! System initialized. Waiting for RFID tags..."));
 }
 
 
@@ -198,4 +204,26 @@ void activateRelay()
   digitalWrite(OPEN_LED_PIN, HIGH);
   digitalWrite(CLOSED_LED_PIN, LOW);
   isLocked = false;
+}
+
+
+
+void setDeviceOutputs()
+{
+  //Warning the device is ready to lock
+  DEBUG_PRINTLN("Device locking in...");
+  for (int x = 3; x >= 0; x--)
+  {
+    digitalWrite(OPEN_LED_PIN, HIGH);
+    digitalWrite(CLOSED_LED_PIN, HIGH);
+    delay(DELAY_AMT);
+    digitalWrite(OPEN_LED_PIN, LOW);
+    digitalWrite(CLOSED_LED_PIN, LOW);
+    DEBUG_PRINTLN(x);
+  }
+
+  //Initial state of outputs set
+  analogWrite(PWM_OUT_PIN, PWM_LOW_VAL);
+  digitalWrite(OPEN_LED_PIN, LOW);
+  digitalWrite(CLOSED_LED_PIN, HIGH);
 }
